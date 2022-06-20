965c7fcc5fd0715f4fb7d4539d3c8ea5a8d0cbad
Gabriel <bellsena@outlook.com>
2022-06-20 10:12:18 -0300

    finallizando quarta aula

---
+++ api/user/resolvers/userResolvers.js
@@ -0,0 +1,17 @@
+const userResolvers = {
+  Query: {
+    users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
+    user: (root, { id }, { dataSources }) =>
+      dataSources.usersAPI.getUserById(id),
+  },
+  Mutation: {
+    criaUser: async (root, user, { dataSources }) =>
+      dataSources.usersAPI.criaUser(user),
+    atualizaUser: async (root, novosDados, { dataSources }) =>
+      dataSources.usersAPI.atualizaUser(novosDados),
+    deletaUser: async (root, { id }, { dataSources }) =>
+      dataSources.usersAPI.deletaUser(id),
+  },
+};
+
+module.exports = userResolvers;
